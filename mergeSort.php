e75f2e90e099da9f9fe35abaf49cb6a49b12f7c9
DANYA <faainttt@gmail.com>
2015-11-07 01:54:22 +0300

    mergeSort

---
+++ mergeSort.php
@@ -0,0 +1,28 @@
+<?php
+
+function mergeSort($arr) {
+    if(count($arr)>1) {
+
+        $middleVal = floor(count($arr)/2);
+        $arr1 = mergeSort(array_slice($arr, 0, $middleVal));
+        $arr2 = mergeSort(array_slice($arr, $middleVal, count($arr)));
+        $counter1 = $counter2 = 0;
+
+        for ($i=0; $i<count($arr); $i++) {
+            if($counter1 == count($arr1)) {
+                $arr[$i] = $arr2[$counter2];
+                $counter2++;
+            } elseif (($counter2 == count($arr2)) or ($arr1[$counter1] < $arr2[$counter2])) {
+                $arr[$i] = $arr1[$counter1];
+                $counter1++;
+            } else {
+                $arr[$i] = $arr2[$counter2];
+                $counter2++;
+            }
+        }
+    }
+    return $arr;
+}
+$arr = array(2,3,7,64,2,3,5,4,6, 9);
+var_dump($arr);
+var_dump(mergeSort($arr));
